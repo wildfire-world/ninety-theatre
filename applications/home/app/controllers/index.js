6b38645dcaeb6383c35ed629adc3ec3bf033c6ff
meenu-yaduvanshi <meenuyaduvanshi24@gmail.com>
2025-07-17 10:46:49 +0000

    view type buton ui change

--- applications/home/app/controllers/index.js
+++ applications/home/app/controllers/index.js
@@ -2,32 +2,41 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import ScrollReveal from 'scrollreveal';
 
 
 export default class IndexController extends Controller {
   @service store;
   briefVisible = true;
-    @tracked viewData = 'listform'; // Default is 'list'
+  @tracked viewData = 'listform'; // Default 'list'
+  @tracked selectedYear = [1989];
 
-  @tracked selectedYear = 1989;
+  @tracked activeYear = 1989;
 
   years = [1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000];
 
   @action
-  selectYear(year) {
-    // console.log(year)
-    this.selectedYear = year;
+  handleActiveYear(year) {
+    // console.log(this.selectedYear)
+    this.activeYear = year;
+    if (!this.selectedYear.includes(year)) {
+      this.selectedYear = [...this.selectedYear, year];
+    }
+
+    this.selectedYear.forEach((year) => {
+  console.log("Visited year:", year);
+});
+
+
   }
 
   @action
   handleListview() {
-        this.viewData = 'listform';
+    this.viewData = 'listform';
   }
 
   @action
   handleCardview() {
-        this.viewData = 'cardform';
+    this.viewData = 'cardform';
   }
 
 }
